14ecbbbbeef631b191ff4ee6cfe11273d7515cad
minervatea <minju.kang@outlook.com>
2022-05-07 10:18:47 +1000

    inital settings done

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    }
+  },
+  extends: ['plugin:prettier/recommended', 'plugin:jest-dom/recommended', 'react', 'prettier'],
+  rules: { 'prettier/prettier': ['error', { endOfLine: 'auto' }] },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: ['react', 'testing-library', 'jest-dom', '@typescript-eslint'],
+  globals: {
+    google: 'readonly'
+  }
+};
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+    return <div>Damoa Front
+        end Testing</div>
+}
+
+export default App;
