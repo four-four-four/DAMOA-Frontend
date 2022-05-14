c3b28276cdda640020687c78db89f1ef2e01246c
wnsgml351 <wnsgml351@gmail.com>
2022-05-14 11:11:02 +0900

    메인 페이지, 컨테이너 추가

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainContainer = () => {
+
+    return (
+        <>
+            <h1>메인 컨테이너</h1>
+        </>
+    );
+};
+
+export default MainContainer;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+// main page
+import React from 'react';
+
+const MainPage = () => {
+
+    return (
+        <>
+            <h1>메인 페이지</h1>
+        </>
+    )
+};
+
+export default MainPage;
