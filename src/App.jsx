42a53fa28d52c49104d2b6c1954bd6cd65f0d70a
wnsgml351 <wnsgml351@gmail.com>
2022-05-28 11:11:25 +0900

    메인 페이지 mui 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Routes from './Routes';
+
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import LoginIcon from '@mui/icons-material/Login';
+import Link from '@mui/material/Link';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export default function App() {
   return (
@@ -10,9 +19,44 @@ export default function App() {
           borderBottom: 'solid 1px',
           padding: '2rem'
         }}>
-        <Link to="/">Main</Link>
+
+        <AppBar position="static">
+            <Toolbar variant="dense">
+                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                
+                </IconButton>
+                <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, alignSelf: 'flex-end', margin: 'auto' }}>
+                    <Link href="/" color="inherit" underline="none" sx={{marginRight: 2}}>
+                        홈
+                    </Link>
+                    <Link href="/" color="inherit" underline="none" sx={{marginRight: 2}}>
+                        트렌드
+                    </Link>
+                    <Link href="/Notice" color="inherit" underline="none" sx={{marginRight: 2}}>
+                        공지사항
+                    </Link>
+                    <Link href="/mypage" color="inherit" underline="none">
+                        마이 페이지
+                    </Link>
+                </Typography>
+
+                <IconButton
+                    size="large"
+                    aria-label="display more actions"
+                    edge="end"
+                    color="inherit"
+                >
+                    {/* 로그인 전 아이콘 */}
+                    <LoginIcon />
+                    {/* 로그인 후 아이콘 */}
+                    {/* <ArrowDropDownIcon /> */}
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+
+        {/* <Link to="/">Main</Link>
         <Link to="/notice">Notice</Link>
-        <Link to="/mypage">My Page</Link>
+        <Link to="/mypage">My Page</Link> */}
       </nav>
       <Routes />
     </div>
