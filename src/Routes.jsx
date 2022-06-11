b73e4b76404a71f1b7f13166b23bbfec2e4656b7
권유리 <99jiyusu@gmail.com>
2022-06-11 11:42:13 +0900

    현재 작업까지 푸쉬(테이블 생성)

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,11 +3,15 @@ import { Route, Routes as Switch } from 'react-router-dom';
 import { PATH_ROOT, PATH_USER_LOGIN, PATH_MY_PAGE, PATH_NOTICE } from './constant/pathConstant';
 import MainPage from './pages/MainPage';
 import MyPage from './pages/MyPage/index';
+import NoticePage from './pages/NoticePage';
+
 
 const Routes = () => (
   <Switch>
     <Route path={PATH_ROOT} element={<MainPage />} />
     <Route path={PATH_MY_PAGE} element={<MyPage />} />
+    <Route path={PATH_NOTICE} element={<NoticePage />} />
+
   </Switch>
 );
 
